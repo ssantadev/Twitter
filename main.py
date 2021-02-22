9b77d1012c5be061b79e400d153ec01c8bb53e39
Samuel Santa Arias <samuel@ssanta.dev>
2021-02-22 14:03:03 -0500

    Añadido el Feature para Identificar por Posición las Tendencias y la Cantidad de Twits que quieras mostrar por cada tendencia

--- main.py
+++ main.py
@@ -14,12 +14,18 @@ api = tweepy.API(auth)
 trendingtweet = api.trends_place(1)
 
 # Where On Earth ID for Colombia is 23424768.
-WOE_ID = 1
+WOE_ID = 368148
 
 
 # COUNTRIES WOE_ID
 
+print("Ingresa la posición de la Tendencia que quieras ver")
+tendencyInput = int(input())
+tendencyPlace = tendencyInput - 1
 
+print("Ingresa la cantidad de Twits de esta tendencia que quieras ver")
+twitsNumber = int(input())
+twitsPlace = twitsNumber
 region_trends = api.trends_place(WOE_ID, wait_on_rate_limit=True)
 
 trends = json.loads(json.dumps(region_trends, indent=1))
@@ -28,9 +34,9 @@ print("Las tendencias más importantes de la región son: ")
 print(f"{trends}  \n")
 
 print("Los Twits más importantes de la tendencia son: \n")
-trendingtopic = trends[0]['trends'][3]['name']
+trendingtopic = trends[0]['trends'][tendencyPlace]['name']
 search_hashtags = tweepy.Cursor(
-    api.search, q=trendingtopic, result_type="popular").items(10)
+    api.search, q=trendingtopic, result_type="popular").items(twitsPlace)
 for trend in search_hashtags:
     res = trend._json
     print(f"TEXTO: {res['text']} "
